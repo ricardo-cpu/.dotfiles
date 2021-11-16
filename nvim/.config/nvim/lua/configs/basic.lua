64abef40c32d03554ad758dd07563e2668a5e8e9
Ricardo alves da silva <earthdoor5702291@gmail.com>
2021-11-16 17:04:00 -0400

    add embed markdown highlight

--- nvim/.config/nvim/lua/configs/basic.lua
+++ nvim/.config/nvim/lua/configs/basic.lua
@@ -80,6 +80,7 @@ vim.cmd([[autocmd InsertEnter * setl norelativenumber]])
 vim.cmd([[ syntax enable ]])
 vim.cmd([[ syntax on ]])
 vim.api.nvim_set_var( "vimsyn_embed", "lPr" )
+vim.g["markdown_fenced_languages"] = {'html', 'python', 'ruby', 'vim', 'javascript', 'json', 'css'}
 
 -- Let 'tl' toggle between this and the last accessed tab
 vim.api.nvim_set_var('lasttab', 1)
