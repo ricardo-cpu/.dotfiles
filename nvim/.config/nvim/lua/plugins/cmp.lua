c071808a04ebccb3de4f48da6dbba0c3e79c85d7
Ricardo alves da silva <earthdoor5702291@gmail.com>
2021-12-16 22:23:38 -0400

    fix bug in cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -14,6 +14,7 @@ cmp.setup {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-Space>"] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
         { name = "nvim_lsp" },
