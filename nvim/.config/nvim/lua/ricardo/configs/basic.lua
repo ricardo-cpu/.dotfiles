3d3245962dcbf49f0ab2e854c5a088278af28295
Ricardo Alves da Silva <ricardcpu@gmail.com>
2022-01-16 23:39:42 -0400

    set statusline without plugins

--- nvim/.config/nvim/lua/ricardo/configs/basic.lua
+++ nvim/.config/nvim/lua/ricardo/configs/basic.lua
@@ -93,3 +93,16 @@ vim.g["markdown_fenced_languages"] = {'html', 'python', 'ruby', 'vim', 'javascri
 -- Let 'tl' toggle between this and the last accessed tab
 vim.api.nvim_set_var('lasttab', 1)
 vim.cmd([[au TabLeave * let g:lasttab = tabpagenr()]])
+
+-- statusline without plugin
+function GitBranch()
+    local handle = io.popen("git rev-parse --abbrev-ref HEAD 2>/dev/null")
+    local result = handle:read("*a")
+    handle:close()
+    result = result:gsub("[\n\r]", "")
+    return result
+end
+
+-- vim.cmd([[ au BufWinEnter * lua vim.g.git_branch = " " .. GitBranch() .. " "]])
+-- vim.cmd([[ au BufWinEnter * setl statusline=%#TabLineSel#%f\ %h%w%m%r\ %#QuickFixLine#%{&clipboard}%#TabLineSel#\ %=%(%l,%c%V%=\ %p%%%#DiagnosticError#%{g:git_branch}%{g:asyncrun_status}%) ]])
+
